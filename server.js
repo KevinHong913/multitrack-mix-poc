0cab1525a9be1f8942323d635c5f915f9133d368
KevinHong <hungwe55@gmail.com>
2016-09-04 15:26:15 -0700

    add slider, start, stop

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-var express = require('express'), app = express();
+var express = require('express');
+var app = express();
+// var path = require('path');
 
 app.use(express.static('app'));
+// app.use('bower_components',  express.static( path.join(__dirname, '/bower_components')));
+// app.use(express.static('bower_components'));
+
 app.set('port', process.env.PORT || 7000);
-app.listen(process.env.PORT || 5000, function () {
+app.listen(process.env.PORT || 7000, function () {
 	    console.log('Express server listening on port ' + app.get('port'));
 });
 
